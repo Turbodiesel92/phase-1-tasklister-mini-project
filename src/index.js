dc8980d0728546b37bb9e9d8db0ba622bd122fc7
Matthew <turbodieseldodge@gmail.com>
2022-12-15 11:20:55 -0600

    Task Lister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+
+  const form = document.querySelector ("#create-task-form")
+
+  form.addEventListener("submit", (e) => createNewTask(e))
+
+  const createNewTask = (e) => {
+    e.preventDefault()
+    const tasksDesc = document.querySelector("#new-task-description").value
+    const newLi = document.createElement("li")
+    newLi.innerHTML = tasksDesc
+    const tasks = document.querySelector("#tasks")
+    tasks.appendChild(newLi)
+    const delteBtn = document.createElement('button')
+    delteBtn.innerText = 'Completed'
+    newLi.append(delteBtn)
+
+    delteBtn.addEventListener("click", (e) => {
+      const removingLi = e.target.parentNode
+      console.log(removingLi)
+      removingLi.remove()
+    })
+
+    e.target.reset()
+  }
+})
+
+
+
+
+
+
+
+
+
